beae4ddfe87ece30a30b28e7a6d5df9a79861d26
MarjolijnK <marjolijn.knops@gmail.com>
2020-02-10 11:48:23 +0100

    added search function

--- src/app/festivals/festivals.component.ts
+++ src/app/festivals/festivals.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Festival } from 'src/app/shared/model/festival.model'
 import { FestivalService } from 'src/app/shared/services/festival.service';
-import { Observable, from } from 'rxjs';
-import { first, take } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs';
+import { debounce } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-festivals',
@@ -12,12 +13,35 @@ import { first, take } from 'rxjs/operators';
 export class FestivalsComponent implements OnInit {
 
   public festivals$: Observable<Festival[]>;
+  
+  public results: Festival[];
   public selected: Festival;
+
+  public search: FormControl = new FormControl();  
 
   constructor(private festivalService: FestivalService) { }
 
   ngOnInit() {
     this.festivals$ = this.festivalService.getFestivals();
+    this.resetList();
+
+    this.search.valueChanges
+      .pipe(debounce(() => interval(100)))
+      .subscribe((query: string) => {
+        console.log('query:', query);
+        if (!query) {
+          this.resetList();
+        }
+
+        const regex = new RegExp(this.escapeRegExp(query), 'i');
+        this.festivals$.subscribe(items => {
+          this.results = items.filter(item => (
+               regex.test(item.name)
+            || regex.test(item.country)
+            || regex.test(item.city)
+          ));
+        });
+      });
   }
 
   addFav(id: number) {
@@ -30,6 +54,16 @@ export class FestivalsComponent implements OnInit {
     this.selected = festival;
   }
 
+  resetList() {
+    this.festivals$.subscribe(items => this.results = items);
+  }
+
+  /**
+   * Stolen from; https://stackoverflow.com/a/6969486
+   * @param string String to escape for regex
+   */
+  escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+  }
+
 }
-
-
